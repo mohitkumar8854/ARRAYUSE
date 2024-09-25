bf6059914f0e5592e1a32b7c97c2ba8df5f11bc7
Mohit Kumar <120978481+mohitkumar8854@users.noreply.github.com>
2024-09-25 15:27:57 +0530

    Add files via upload

---
+++ InputArray.java
@@ -0,0 +1,33 @@
+package array;
+//import java.util.*;
+
+//public class InputArray{
+//	int eid;
+//	String ename;
+//	int sal;
+//	void accept()
+//	{
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter the employe ID :");
+//		eid=sc.nextInt();
+//		sc.nextLine();
+//		System.out.println("Enter the employe name :");
+//		 ename=sc.nextLine();
+//		System.out.println("enter the Employe salary:");
+//		sal=sc.nextInt();
+//	}
+//	void disp()
+//	{
+//		System.out.println("Employe ID:"+eid);
+//		System.out.println("Employe ename:"+ename);
+//		System.out.println("Employe Salary:"+sal);
+//	}
+//	public static void main(String arg[])
+//	{
+//		InputArray i1=new InputArray();
+//		i1.accept();
+//		i1.disp();
+//	}
+//}
+//
+
